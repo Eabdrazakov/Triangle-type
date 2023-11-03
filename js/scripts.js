4247c71607a95697b89a13f4b304e82ae9fc3171
Padma Patil <padma@email.com>
2023-11-03 11:41:42 -0700

    Add eventlintener

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,26 @@ function processFormValues() {
     const name2 = parseInt(document.querySelector("input#name2").value);
     const name3 = parseInt(document.querySelector("input#name3").value);
 
-    if (name1 > name2 + name3) {
-        document.getElementById("equi").removeAttribute("class");
-    } else if (name2 > name1 + name3) {
-        document.getElementById("isos").removeAttribute("class");
-    } else if (name3 > name1 + name2) {
-        document.getElementById("scal").removeAttribute("class");
-    } else {
-        document.getElementById("sorry").removeAttribute("class");
+    if (name1 && name2 && name3) {
+
+        if (name1 === name2 && name1 === name3) {
+            document.getElementById("equi").removeAttribute("class");
+        } else if (name2 !== name1 || name2 === name3) {
+            document.getElementById("isos").removeAttribute("class");
+        } else if (name3 !== name1 || name3 !== name2) {
+            document.getElementById("scal").removeAttribute("class");
+        } else {
+            document.getElementById("sorry").removeAttribute("class");
+        }
     }
 }
+
+window.addEventListener("load", function () {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        hideResults();
+        processFormValues();
+        event.preventDefault();
+
+    });
+});
