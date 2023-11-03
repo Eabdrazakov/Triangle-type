0950194da52e143b4a43f72dbc24ae97c64553a0
Padma Patil <padma@email.com>
2023-11-03 11:19:03 -0700

    Add js if and else

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+function hideResults() {
+    document.getElementById("equi").setAttribute("class", "hidden");
+    document.getElementById("isos").setAttribute("class", "hidden");
+    document.getElementById("scal").setAttribute("class", "hidden");
+    document.getElementById("sorry").setAttribute("class", "hidden");
+}
+
+function processFormValues() {
+    hideResults();
+    const name1 = parseInt(document.querySelector("input#name1").value);
+    const name2 = parseInt(document.querySelector("input#name2").value);
+    const name3 = parseInt(document.querySelector("input#name3").value);
+
+    if (name1 > name2 + name3) {
+        document.getElementById("equi").removeAttribute("class");
+    } else if (name2 > name1 + name3) {
+        document.getElementById("isos").removeAttribute("class");
+    } else if (name3 > name1 + name2) {
+        document.getElementById("scal").removeAttribute("class");
+    } else {
+        document.getElementById("sorry").removeAttribute("class");
+    }
+}
